22796503deb3eeb8a1e6a3be947446ff6ec19075
xitengfei <tengfei.xi@qq.com>
2019-11-13 10:18:17 +0800

    update

--- test/behavesLikeSqlFormatter.js
+++ test/behavesLikeSqlFormatter.js
@@ -465,4 +465,17 @@ export default function behavesLikeSqlFormatter(language) {
             "  Table2;"
         );
     });
+
+    it('formats unicode correctly', function() {
+        const result = format(
+            'SELECT test, тест FROM table;'
+        );
+        expect(result).toBe(
+            'SELECT\n' +
+            '  中文,\n' +
+            '  тест\n' +
+            'FROM\n' +
+            '  table;'
+        );
+    });
 }
